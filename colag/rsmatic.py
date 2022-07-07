6b48d7c2c38a681ccaee8677e9e83b42ebb80169
peitur <peitur@gmail.com>
2022-07-07 09:34:59 +0200

    upadted

--- colag/rsmatic.py
+++ colag/rsmatic.py
@@ -4,7 +4,7 @@
 import sys,os,re
 import json
 import colag
-import colag.validate.SimpleDictValidator as Validator
+# import colag.validate.SimpleDictValidator as Validator
 
 class RsmaticConfig( object ):
     
@@ -13,18 +13,71 @@ class RsmaticConfig( object ):
         self._filename = filename
         self._data = None
         
-        self._valid_config = {
-            "debug":{""},
-            "source":{},
-            "target":{}
-        }
-        self._valid_coptions = {}
+        self._static_options = [
+            "--no-motd",
+            "--recursive"
+        ]
         
+        
+        self._valid_config = {
+            "debug":{"type":"bool", "mandatory": False },
+            "source":{ "type":"str", "pattern":r".+", "mandatory":True },
+            "target":{ "type":"str", "pattern":r".+", "mandatory":True },
+            "options":{"type":"list", "mandatory": False, "pattern": None,  }
+        }
+        
+        self._valid_options = {
+            "port":{"mandatory": False, "pattern": r"^[0-9]+$" , "type":"int"  },
+            "ipv4":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "ipv6":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "list-only":{"mandatory": False, "pattern": None , "type":"flag"  },            
+            "checksum":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "recursive":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "archive":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "links":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "acls":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "xattrs":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "owner":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "group":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "times":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "dry-run":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "delete":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "delete-after":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "delete-before":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "delete-during":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "delete-delay":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "delete-excluded":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "force":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "ignore-times":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "size-only":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "compress":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "compress-level":{"mandatory": False, "pattern": r"^[0-9]+$" , "type":"int"  },
+            "skip-compress":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "exclude":{"mandatory": False, "pattern": ".+" , "type":"list"  },
+            "exclude-from":{"mandatory": False, "pattern": ".+" , "type":"str"  },
+            "include":{"mandatory": False, "pattern": ".+" , "type":"list"  },
+            "include-from":{"mandatory": False, "pattern": ".+" , "type":"str"  },
+            "bwlimit":{"mandatory": False, "pattern": r"^[0-9]+$" , "type":"int"  },
+            "protect-args":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "contimeout":{"mandatory": False, "pattern": "^[0-9]+$" , "type":"int"  },
+            "numeric-ids":{"mandatory": False, "pattern": None , "type":"flag"  },
+            "min-size":{"mandatory": False, "pattern": r"^[0-9]+$" , "type":"int"  },
+            "max-size":{"mandatory": False, "pattern": r"^[0-9]+$" , "type":"int"  },
+            "delay-updates":{"mandatory": False, "pattern": None , "type":"flag"  }
+        }
+    
+    
     def _load_file( self ):
         cache = json.load( open(self._filename ), "r" )
-
-
-
+        ## temporary until validator works as intended (famous last words)
+        for c in cache:
+            if c not in self._valid_config:
+                raise AttributeError("Unsupported configuration option: %s" % ( c ) )
+            
+        if 'options' in cache and type( cache['options'] ).__name__ in ( "dict" ):
+            for o in cache['options']:
+                if o not in self._valid_options:
+                    raise AttributeError("Unsupported rsync option: %s" % ( c ) )
 
 if __name__ == "__main__":
     pass
