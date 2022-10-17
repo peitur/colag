cb26a949925808e5e26b7374088b146e1ac028e6
vagrant <vagrant@plain91.example.se>
2022-10-17 10:12:25 +0200

    fixed some bugs and did some basic tests

--- rpmrepoat.py
+++ rpmrepoat.py
@@ -14,17 +14,13 @@ from pprint import pprint
 
 if __name__  == "__main__":
     
-    if len( sys.argv ) < 3:
-        raise RuntimeError( "Missing arguments: <config> <destination>" )
+    if len( sys.argv ) < 2:
+        raise RuntimeError( "Missing arguments: <config>" )
     
-    filename = pathlib.Path( sys.argv[1] )
-    destination = pathlib.Path( sys.argv[2] )
+    filename = pathlib.Path( sys.argv[1] )        
+    s = colag.rpmrepo.config_read( filename  )        
+    gen = colag.rpmrepo.RepoSyncConfigGen( s['main'], s['repos'], debug=True )
     
-    if not destination.exists():
-        raise OSError("Missing destination directory %s" % ( destination ) )
-    
-    s = colag.rpmrepo.config_read( filename  )
-    gen = colag.rpmrepo.RepoSyncConfigGen( s['main'], s['repos'], rootdir=destination, debug=True )
     print("##### Full configuration  #####")
     gen.print_all()
     print("###############################")
