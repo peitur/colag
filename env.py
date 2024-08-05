4113898f09a8218a60aba71a3c03b7a2f057c3cd
peitur <peitur@gmail.com>
2024-08-05 16:08:48 +0200

    wip

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-
+import sys
 import json
 import pathlib
 
@@ -9,5 +9,9 @@ CONFIG={
 }
 
 if __name__ == "__main__":
+    infiles = list()
+    if len( sys.argv ) > 1:
+        infiles = sys.argv[ 1: ]
 
+    
     print( json.dumps( CONFIG ) )
