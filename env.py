1238f02c77a38afe46052506f414c1d4d6a3f6e5
peitur <peitur@gmail.com>
2024-08-05 16:23:05 +0200

    wip

--- env.py
+++ env.py
@@ -3,15 +3,22 @@
 import sys
 import json
 import pathlib
+import time
 
 CONFIG={
     
 }
 
 if __name__ == "__main__":
+    conf = CONFIG.copy()
     infiles = list()
     if len( sys.argv ) > 1:
         infiles = sys.argv[ 1: ]
 
+    for f in infiles:
+        if pathlib.Path( f ).suffix == ".json":
+            print( f )
     
-    print( json.dumps( CONFIG ) )
+    print( json.dumps( conf ) )
+    
+        
